2cf8c2305d0eff7a4c8fce8e75a01644b471824a
Shimwa Yvan <shimwayvan@gmail.com>
2022-05-03 15:42:37 +0200

    add new page home.html

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sinarwanda</title>
+    <link rel="stylesheet" href="sinarwa.css">
+</head>
+<body>
+    <div class="header">
+    <img src="sinalogo.png" alt="sina logo" width="130px" height="100px">
+         <ul>
+             <li><a href="home.html">HOME</a></li>
+             <li><a href="aboutus.html">ABOUT US</a></li>
+             <li><a href="gallery.html">GALLERY</a></li>
+             <li><a href="ourshop.html">OUR SHOP</a></li>
+             <li><a href="contact.php">CONTACT</a></li>
+         </ul>
+    </div>
+    <div class="body">
+            <?php
+
+            ?>
+    </div>
+    <div class="footer">
+     <p class="ftr"> Copyright  © 2020 <b>All rights reserved to Entreprise URWIBUTSO.</b></p>
+     <div class="icons">
+        <a href="https://twitter.com/sinarwanda?lang=en"><ion-icon name="logo-twitter" size="large"></ion-icon></a>
+      <a href="https://www.facebook.com/Nyirangarama"><ion-icon name="logo-facebook" size="large"></ion-icon></a>
+      <a href="https://instagram.com/nyirangarama?igshid=vtgtdzzaw9ka#"><ion-icon name="logo-instagram" size="large"></ion-icon></a>
+     </div>
+     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>    
+    </div>
+</body>
+</html>
